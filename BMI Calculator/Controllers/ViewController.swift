209e5638b4261f34faa057305e033b580fc5fb18
coofzilla <jeric614@yahoo.com>
2023-01-22 19:41:47 +0900

    feature: adjust weightLabel based on slider value

--- BMI Calculator/Controllers/ViewController.swift
+++ BMI Calculator/Controllers/ViewController.swift
@@ -12,14 +12,21 @@ class ViewController: UIViewController {
     @IBOutlet weak var heightSlider: UISlider!
     @IBOutlet weak var heightLabel: UILabel!
     
+    @IBOutlet weak var weightLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
     
-    @IBAction func sliderDragged(_ sender: UISlider) {
+    @IBAction func heightSliderActionHandler(_ sender: UISlider) {
         let roundedHeight = round(sender.value * 10) / 10
         heightLabel.text = "\(roundedHeight)m"
+    }
+    
+    @IBAction func weightSliderActionHandler(_ sender: UISlider) {
+        let roundedWeight = round(sender.value * 10) / 10
+        weightLabel.text = "\(roundedWeight)Kg"
     }
     
 }
